483cf4a312cc0778236a5a389d7f9602cf223a73
Jeremy Pollock <jpollock911@gmail.com>
2018-06-21 11:05:21 -0700

    basic port support

--- index.js
+++ index.js
@@ -366,3 +366,12 @@ pubnub.subscribe({
     channels: [readENV('PUBNUB_CHANNEL')]
 });
 
+
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(PORT, () => console.log('Example app listening on port =' + PORT + ' !'))
